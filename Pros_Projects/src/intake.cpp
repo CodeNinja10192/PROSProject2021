588a43540fa39fd62b700a11ddc6f23bd6e6d216
CodeNinja10192 <59780336+CodeNinja10192@users.noreply.github.com>
2021-12-16 15:34:21 -0500

    Intake PID 12/16
    Added PID for Intake, going to work on lift for rest of day, and then hopefully make one final commit and push.

--- Pros_Projects/src/intake.cpp
+++ Pros_Projects/src/intake.cpp
@@ -1,5 +1,5 @@
 #include "main.h"
-
+int intakeTarget = 0;
 //Intake Control (OLD)
 
 /*
@@ -55,6 +55,30 @@ void intakeTask(void* parameter) {
   if (pros::competition::is_connected()) {
     while (true) {
       intakeOpControl();
+      pros::delay(20);
+    }
+  }
+  else if (pros::competition::is_autonomous()) {
+    int previousError = 0;
+    while (true) {
+      int sp = intakeTarget;
+
+      double kP = 0.3;
+      double kI = 0.3;
+      double kD = 0.3;
+
+      int sv = intake.get_position();
+
+      int error = sp - sv;
+      int integral = integral + error;
+      int derivative = error - previousError;
+
+      if (error == 0) {
+        integral = 0;
+      }
+      int power = (error * kP) + (integral * kI) + (derivative * kD);
+      previousError = error;
+      intakePower(power);
       pros::delay(20);
     }
   }
